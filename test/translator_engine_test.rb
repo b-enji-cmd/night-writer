1bf206405b81e638dc38dcedbc838c877bbf03df
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-07 18:09:04 -0700

    Add translator superclass

---
+++ test/translator_engine_test.rb
@@ -0,0 +1,19 @@
+require './lib/parser'
+require './lib/translator_engine'
+require 'minitest/autorun'
+require 'pry'
+
+class TranslatorEngineTest < Minitest::Test
+  def setup
+    @super_translator = TranslatorEngine.new
+  end
+
+  def test_it_is
+    assert_instance_of TranslatorEngine, @super_translator
+  end
+
+  def test_it_starts_with_a_dictionary
+    binding.pry
+    assert_instance_of Array, @super_translator.dictionary.keys.class
+  end
+end
