f934854366f1eedc4f2427a0a1f392db03881341
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-07 21:26:12 -0700

    Add passing translation logic to braille

--- lib/translator_to_braille.rb
+++ lib/translator_to_braille.rb
@@ -1,6 +1,20 @@
 class TranslatorToBraille < TranslatorEngine
+  attr_reader :data
+
   def initialize
     super
+    @data = load_data
+  end
+
+  def translate
+    encoded = Hash.new
+    letters = @data.flat_map do |line|
+      line.split.flat_map(&:chars)
+    end
+    letters.each do |letter|
+      encoded[letter] = @dictionary[letter]
+    end
+    encoded.values
   end
 
 end
--- test/translator_to_braille_test.rb
+++ test/translator_to_braille_test.rb
@@ -14,4 +14,16 @@ class TranslatorToBrailleTest < Minitest::Test
     assert_instance_of TranslatorToBraille, @to_braille
   end
 
+  def test_it_can_get_data_from_parser
+    assert_instance_of Array, @to_braille.data
+  end
+
+  def test_it_can_handle_data
+    assert_instance_of Array, @to_braille.translate
+  end
+
+  def test_it_can_translate_letters_to_braille
+    assert_instance_of String, @to_braille.translate[0][0][0]
+  end
+
 end
