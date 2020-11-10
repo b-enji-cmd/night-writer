8589dfcc1bd5ba08a9dbedb01a6827c6e2d33f84
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-09 20:02:43 -0700

    Add text wrapping

--- lib/translator_to_braille.rb
+++ lib/translator_to_braille.rb
@@ -17,49 +17,75 @@ class TranslatorToBraille < TranslatorEngine
     characters
   end
 
-  def write_encoded
-    first = translate.flatten(1)
+  def assign_rows
+    rows = []
     top = []
     mid = []
     bot = []
-
+    first = translate.flatten(1)
     first.each do |column|
-        top << column[0]
-        mid << column[1]
-        bot << column[2]
+      top << column[0]
+      mid << column[1]
+      bot << column[2]
     end
-    top.append("NEWLINE")
-    mid.append("NEWLINE")
-    bot.append("NEWLINE")
+    top.append("\n")
+    mid.append("\n")
+    bot.append("\n")
+    rows << top << mid << bot
+  end
 
-    collector = []
-    collector << top
-    collector << mid
-    collector << bot
+  def write_encoded
+    translated_data = assign_rows
+    top_row = translated_data[0]
+    middle_row = translated_data[1]
+    bottom_row = translated_data[2]
 
-    collector.each do |row|
-      count = 0
-      row.each do |char|
-        if count == 79
-          @super_parser.write_data("\n")
-        else
-          @super_parser.write_data(char)
-        end
-        count+=1
+    top_slice = []
+    mid_slice = []
+    bot_slice = []
+
+    top_row.each_slice(80) do |split_point|
+      top_slice << split_point
+    end
+    middle_row.each_slice(80) do |split_point|
+      mid_slice << split_point
+    end
+    bottom_row.each_slice(80) do |split_point|
+      bot_slice << split_point
+    end
+
+    if top_row.count > 80
+      top_slice[0].append("\n")
+      mid_slice[0].append("\n")
+      bot_slice[0].append("\n")
+    end
+
+
+    top_slice[0].each do |char|
+      @super_parser.write_data(char)
+    end
+    mid_slice[0].each do |char|
+      @super_parser.write_data(char)
+    end
+    bot_slice[0].each do |char|
+      @super_parser.write_data(char)
+    end
+    if top_row.count > 80
+      top_slice[1].each do |char|
+        @super_parser.write_data(char)
+      end
+      mid_slice[1].each do |char|
+        @super_parser.write_data(char)
+      end
+      bot_slice[1].each do |char|
+        @super_parser.write_data(char)
       end
     end
 
+
   end
 end
-#each with index handles taking the rows
-# have the ling break after the 80th column
-#
 
-
-
-
-
-# potential strategies to refactor this method
-# setting up rake to handle arguments
-# how to test write method
-# best way of handling line lengths that are greater than 80
+#top[80]
+#  mid[80]
+#   bot[80]
--- lib/translator_to_english.rb
+++ lib/translator_to_english.rb
@@ -11,54 +11,54 @@ class TranslatorToEnglish < TranslatorEngine
   end
 
   def translate
+    #
+     top = []
+    # mid = []
+    # bot = []
+     new_dictionary = hash_swap
+     top << @data[0].delete("\n")
+    # mid << @data[1].delete("\n")
+    # bot << @data[2].delete("\n")
+    # @data.delete("\n")
 
+     size = @data[0].delete("\n")
+     number_of_letters = (top.first.length / 2)
+     collect = Hash.new{|h,k| h[k] = []}
+     look_up = []
 
-    collector = []
-    top = []
-    mid = []
-    bot = []
-    new_dictionary = hash_swap
-    top << @data[0].delete("\n")
-    mid << @data[1].delete("\n")
-    bot << @data[2].delete("\n")
-    @data.delete("\n")
+     @data.each do |row|
+       count = 1
+       row = row.delete("\n")
+       until count > number_of_letters
+         collect["letter#{count}"] << row[0..1]
+         arr = row.chars
+         arr.shift(2)
+         row = arr.join
+         count +=1
+       end
+     end
 
+     looks = Hash.new{|h,k| h[k] = []}
+     l_column = []
+     r_column = []
+     county = 0
+     collect.each do |k,v|
+       v.each do |pair|
+         l_column << pair[0]
+         r_column << pair[1]
+         county+=1
+       end
+       looks["#{county}"] << l_column << r_column
+       l_column = []
+       r_column = []
 
-    temp_data = top + mid + bot
-
-    number_of_letters = (top.first.length / 2)
-
-    collect = Hash.new{|h,k| h[k] = []}
-
-    look_up = []
-
-
-    @data.each do |row|
-      count = 1
-      # binding.pry
-      row = row.delete("\n")
-      until count > number_of_letters
-        # binding.pry
-        collect["letter#{count}"] << row[0..1]
-        arr = row.chars
-        arr.shift(2)
-        row = arr.join
-        count +=1
-        #binding.pry
-      end
-     collect
+       #
+     end
+    fancy = ""
+    looks.values.each do |pair|
+      fancy += new_dictionary[pair]
     end
-    l_column = []
-    r_column = []
-    collect.each do |k,v|
-      v.each do |pair|
-        l_column << pair[0]
-        r_column << pair[1]
-      end
-      look_up << l_column << r_column
-      binding.pry
-    end
-    binding.pry
+    @super_parser.write_data(fancy)
   end
 
 
