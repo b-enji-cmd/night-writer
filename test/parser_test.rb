86e4a4344c5c10c3b7cca1cc70c927a008cbc21c
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-09 22:34:09 -0700

    Updated testing to 100% coverage for needed files

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -15,16 +15,19 @@ class ParserTest < Minitest::Test
     assert_instance_of String, @parser.read_data[-1]
   end
 
+  def test_it_can_close_files
+    @parser.parse_file
+    refute @parser.close_file
+  end
+
 
   def test_it_can_write_to_file
-    #test = @parser.parse_file
     test = ".\n0\n."
-
     assert @parser.write_data(test)
   end
 
   def test_it_can_return_word_count_cli
     @parser.parse_file
-    assert_equal "Created 'braille.txt' containing 5 characters", @parser.word_count
+    assert_equal "Created 'braille.txt' containing 7 characters", @parser.word_count
   end
 end
