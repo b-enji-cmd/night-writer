a3ce7794a4131f8d98aae9a404b77051d1769643
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-09 17:09:51 -0700

    Add executeable runner content

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,8 +1,12 @@
 require './lib/parser'
-require './lib/translator_to_braille'
 require './lib/translator_engine'
+require './lib/translator_to_braille'
 require './lib/translator_to_english'
+require 'pry'
 
 night_writer = TranslatorEngine.new
-night_writer.writer
-
+night_writer.load_data
+night_writer.super_parser.word_count
+braille = TranslatorToBraille.new
+braille.translate
+braille.write_encoded
