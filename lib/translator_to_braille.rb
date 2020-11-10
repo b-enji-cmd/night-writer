cc399ffd90f5f0e0ab12a36b96bcb5de35005377
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-09 17:14:19 -0700

    Add restructuring for wrapping prep

--- lib/translator_to_braille.rb
+++ lib/translator_to_braille.rb
@@ -7,43 +7,59 @@ class TranslatorToBraille < TranslatorEngine
   end
 
   def translate
-    encoded = {}
     letters = @data.flat_map do |line|
       line.split.flat_map(&:chars)
     end
+    characters = []
     letters.each do |letter|
-      encoded[letter] = @dictionary[letter]
+      characters << @dictionary[letter]
     end
-    encoded.values
+    characters
   end
 
   def write_encoded
-    first = translate
-    second = first.flatten(1)
+    first = translate.flatten(1)
     top = []
     mid = []
     bot = []
-    second.each_with_index do |column, iteration|
-      if iteration == 80
-        @super_parser.write_data("\n")
-      else
+
+    first.each do |column|
         top << column[0]
         mid << column[1]
         bot << column[2]
+    end
+    top.append("NEWLINE")
+    mid.append("NEWLINE")
+    bot.append("NEWLINE")
+
+    collector = []
+    collector << top
+    collector << mid
+    collector << bot
+
+    collector.each do |row|
+      count = 0
+      row.each do |char|
+        if count == 79
+          @super_parser.write_data("\n")
+        else
+          @super_parser.write_data(char)
+        end
+        count+=1
       end
     end
-    top.append("\n")
-    mid.append("\n")
-    bot.append("\n")
 
-    top.each do |char|
-      @super_parser.write_data(char)
-    end
-    mid.each do |char|
-      @super_parser.write_data(char)
-    end
-    bot.each do |char|
-      @super_parser.write_data(char)
-    end
   end
 end
+#each with index handles taking the rows
+# have the ling break after the 80th column
+#
+
+
+
+
+
+# potential strategies to refactor this method
+# setting up rake to handle arguments
+# how to test write method
+# best way of handling line lengths that are greater than 80
